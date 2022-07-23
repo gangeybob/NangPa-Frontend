7f5c380c38f3a92594b26ad5148ac79f332cbf74
Sangjo LEE <cent7425@gmail.com>
2022-07-24 03:47:34 +0900

    feat: :sparkles: recipe detail 페이지의 사진 및 sns 스타일 작업 (#3)

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Main from '../pages/Main';
-import Search from '../pages/Search';
+import Detail from "../pages/Detail";
+import Search from "../pages/Search";
 
 const RootRoute = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Search />} />
-                {/* <Route path='/search' element={<Search />} /> */}
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Search />} />
+        {/* <Route path='/search' element={<Search />} /> */}
+        <Route path="/:recipeId/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default RootRoute;
