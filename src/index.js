70b148249fb185d3f40416d06987fa493197af19
sjoleee <cent7425@gmail.com>
2022-07-24 03:50:35 +0900

    feat: :sparkles: recoil 설치 및 ingredient 컴포넌트 생성, 선택된 재료와 레시피 재료를 비교하여 스타일 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import App from "./App";
+import MainLayout from "./components/MainLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <MainLayout>
+        <App />
+      </MainLayout>
+    </RecoilRoot>
   </React.StrictMode>
 );
