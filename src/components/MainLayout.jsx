a288c6d6c9a072e53234ea80f534753057284da9
sjoleee <cent7425@gmail.com>
2022-07-24 03:42:58 +0900

    feat: :sparkles: recipe detail 페이지의 사진 및 sns 스타일 작업

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledMainLayout = styled.div`
+  position: relative;
+  max-width: 768px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const MainLayout = ({ children }) => {
+  return <StyledMainLayout>{children}</StyledMainLayout>;
+};
+
+export default MainLayout;
--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -5,10 +5,10 @@ const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
 }
-html, body {
-    width:100%;
-    height:100%;
+ body {
     font-family: 'suit variable', sans-serif;
+    margin:0;
+    padding:0;
   }
 
   a {
