10e481a691b357dd05c66cf05eea31a71690c04d
fj <function.fj@gmail.com>
2022-07-23 23:31:56 +0900

    test

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Main from '../pages/Main';
+import Search from '../pages/Search';
 
 const RootRoute = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Search />} />
+                {/* <Route path='/search' element={<Search />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default RootRoute;
