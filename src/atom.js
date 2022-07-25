fe4f1bb35e6a4c3608ea6cb7c09367a857e2c98d
moseung <tmdah900@naver.com>
2022-07-25 23:29:26 +0900

    adad

--- src/atom.js
+++ src/atom.js
@@ -4,11 +4,6 @@ export const selectedIngredientAtom = atom({
   default: [],
 });
 
-export const myFrigeAtom = atom({
-  key: "myFrige",
-  default: [],
-});
-
 export const viewedRecipeAtom = atom({
   key: "viewedRecipe",
   default: [],
