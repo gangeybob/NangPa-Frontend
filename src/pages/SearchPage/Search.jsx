ed494bd619de9c8ebcf45b7fc825a0618564fe1d
moseung <tmdah900@naver.com>
2022-07-27 16:22:20 +0900

    ✅ sinked

--- src/pages/SearchPage/Search.jsx
+++ src/pages/SearchPage/Search.jsx
@@ -118,6 +118,7 @@ const RecipeSearchButton = styled.div`
   cursor: pointer;
   text-align: center;
 `;
+//dd
 function SearchIndex() {
   const [searchInput, setSearchInput] = useState("");
   const [selectedIngredient, setSelectedIngredient] = useRecoilState(
